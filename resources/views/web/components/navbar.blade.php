f6df1d50ae42b4180d3d5e53327a32030cec949c
asepmedia <38859211+asepmedia@users.noreply.github.com>
2022-07-24 21:23:29 +0700

    feat: add model relations

---
+++ resources/views/web/components/navbar.blade.php
@@ -0,0 +1,24 @@
+<div class="shadow">
+    <nav class="container border-bottom-theme bg-white">
+        <div class="row">
+        <div class="col-md-12 d-none d-lg-block">
+            <div class="d-flex justify-content-between align-items-center" style="height: 70px">
+                <div class="align-self-center w-15">
+                    <h4>Sepatuku Store</h4>
+                </div>
+                <div class="w-65" style="width: 65%">
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Cari sepatu...  " aria-label="Search">
+                        <button class="btn btn-outline-dark" type="submit">
+                            <i class="fa fa-fw fa-search text-dark mr-2"></i>
+                        </button>
+                    </form>
+                </div>
+                <div>
+                    <a href="#" class="text-dark">Masuk</a> / <a href="" class="text-secondary">Daftar</a>
+                </div>
+            </div>
+        </div>
+        </div>
+    </nav>
+</div>
