5308435f4d3d9a2078aa6c086d3e0a27e015a76f
asepmedia <38859211+asepmedia@users.noreply.github.com>
2022-07-24 20:28:59 +0700

    feat: add migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,72 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        // - + >
+        // ->
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'member'])->default("member");
             $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->string('name');
+            $table->string('description');
+            $table->string('slug');
+            $table->string('image')->nullable();
+            $table->integer('stock')->default("0");
+            $table->integer('weight')->default("0");
+            $table->integer('length')->default("0");
+            $table->integer('width')->default("0");
+            $table->integer('height')->default("0");
+            $table->enum('status', ['publish', 'draft'])->default('draft');
+            $table->timestamps();
+        });
+
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('product_id')->nullable();
+            $table->integer('length')->default("0");
+            $table->timestamps();
+        });
+
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('order_number');
+            $table->enum('status', ['pending', 'paid', 'process', 'shipping', 'complete', 'cancel'])->default('pending');
+            $table->timestamps();
+        });
+
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id')->nullable();
+            $table->unsignedBigInteger('product_id')->nullable();
+            $table->integer('qty')->default("0");
+            $table->integer('total')->default("0");
+            $table->timestamps();
+        });
+
+        Schema::create('order_shipments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id')->nullable();
+            $table->string('receiver_name');
+            $table->string('receiver_phone');
+            $table->string('receiver_address');
             $table->timestamps();
         });
     }
@@ -32,5 +91,11 @@ class CreateUsersTable extends Migration
     public function down()
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('carts');
+        Schema::dropIfExists('orders');
+        Schema::dropIfExists('order_details');
+        Schema::dropIfExists('order_shipments');
     }
 }
